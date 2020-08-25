3f397d9c660d6dbcc514618a1115d14c81b3e6a0
David Cavar <david.cavar@morescreens.net>
2020-08-25 12:41:44 +0200

    Move createdAt and updatedAt properties to Timestampable trait

--- src/Doctrine/Behaviour/Timestampable.php
+++ src/Doctrine/Behaviour/Timestampable.php
@@ -2,6 +2,24 @@
 namespace RestOnPhp\Doctrine\Behaviour;
 
 trait Timestampable {
+    protected $createdAt, $updatedAt;
+    
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($value) {
+        $this->createdAt = $value;
+    }
+
+    public function getUpdatedAt() {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt($value) {
+        $this->updatedAt = $value;
+    }
+
     public function updatedAt() {
         $this->setUpdatedAt(new \DateTime());
     }
